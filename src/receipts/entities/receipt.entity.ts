173c074277757309e139c3621f330c85ac6f691a
Ty-168 <kitylim123@gmail.com>
2026-03-11 11:40:24 +0700

    initial commit

---
+++ src/receipts/entities/receipt.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('receipts')
+export class Receipt {
+
+    @PrimaryGeneratedColumn('uuid')
+    receiptId: string;
+
+    @Column()
+    issuedAt: Date;
+
+    @Column()
+    name: string;
+
+    @Column()
+    price: number;
+}
